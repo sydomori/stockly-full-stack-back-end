798d97da94d968e663dedc1bf7bb30ec0bc58163
sydomori <sydneyomori@gmail.com>
2026-07-29 02:56:01 +0300

    register the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from app.extensions import db, migrate, jwt, cors
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -13,5 +14,8 @@ def create_app():
 
     with app.app_context():
         from app import models
+
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp)
 
     return app

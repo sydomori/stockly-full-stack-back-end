68705af082a8fe7302f17225adf5c81d15112a3a
sydomori <sydneyomori@gmail.com>
2026-07-29 00:05:08 +0300

    feat: create product_supplier join table

---
+++ app/models/Product_suplier.py
@@ -0,0 +1,14 @@
+from app.extensions import db
+
+class ProductSupplier(db.Model):
+    __tablename__ = 'product_suppliers'
+
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
+    supplier_id = db.Column(db.Integer, db.ForeignKey('suppliers.id'), primary_key=True)
+    cost_price = db.Column(db.Float, nullable=True)
+
+    product = db.relationship('Product', backref='product_suppliers')
+    supplier = db.relationship('Supplier', backref='product_suppliers')
+
+    def __repr__(self):
+        return f'<ProductSupplier product={self.product_id} supplier={self.supplier_id}>'
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.User import User
 from app.models.Category import Category
 from app.models.Supplier import Supplier
 from app.models.Product import Product
+from app.models.Product_suplier import ProductSupplier

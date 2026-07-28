f3a4581cc985b79a0959e05ae186f3541e6579d8
sydomori <sydneyomori@gmail.com>
2026-07-28 10:59:49 +0300

    feat: initialize extensions in extensions.py without binding

---
+++ app/extensions.py
@@ -0,0 +1,12 @@
+#create extension objects for use in app
+#helps avoid circular imports
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+cors = CORS

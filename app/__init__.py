c2a2482ffcd8fb2f18ca3af0f15bf0f3913e092d
sydomori <sydneyomori@gmail.com>
2026-07-29 18:32:59 +0300

    feat: install and configure marshmallow for serialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from app.extensions import db, migrate, jwt, cors
+from app.extensions import db, migrate, jwt, cors, ma
 
 
 def create_app():
@@ -11,6 +11,8 @@ def create_app():
     migrate.init_app(app, db)
     jwt.init_app(app)
     cors.init_app(app)
+    ma.init_app(app)
+    
 
     with app.app_context():
         from app import models

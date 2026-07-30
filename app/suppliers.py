abd8b264f3f45ed808d72e70ff97b244aa832700
sydomori <sydneyomori@gmail.com>
2026-07-30 12:05:46 +0300

    feat: create get single supplier route

--- app/suppliers.py
+++ app/suppliers.py
@@ -14,3 +14,12 @@ suppliers_bp = Blueprint('suppliers', __name__, url_prefix='/suppliers')
 def get_suppliers():
     suppliers = Supplier.query.all()
     return jsonify(suppliers_schema.dump(suppliers)), 200
+
+#get single supplier
+@suppliers_bp.route('/<int:supplier_id>', methods=['GET'])
+@jwt_required()
+def get_supplier(supplier_id):
+    supplier = Supplier.query.get(supplier_id)
+    if not supplier:
+        return jsonify ({'error':'Supplier not found'}),400
+    return jsonify(supplier_schema.dump(supplier)), 200

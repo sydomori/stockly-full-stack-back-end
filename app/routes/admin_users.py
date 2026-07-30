298463024e830be9bcc01bcf9ccf4b7ebb5a7a1e
sydomori <sydneyomori@gmail.com>
2026-07-30 12:49:26 +0300

    feat: create admin users blueprint

---
+++ app/routes/admin_users.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.extensions import db
+from app.models.User import User
+from app.utils import log_activity
+from app.schemas import user_schema, users_schema
+from app.routes.test import admin_required
+import secrets
+
+admin_users_bp = Blueprint('admin_users', __name__, url_prefix='/admin/users')
+

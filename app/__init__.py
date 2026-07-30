382e8f1f8a993d22bd0cface61d41fd6f12097fb
sydomori <sydneyomori@gmail.com>
2026-07-30 12:35:15 +0300

    register products, category and suppliers blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import Config
 from app.extensions import db, migrate, jwt, cors, ma
-
+from app.routes.categories import categories_bp
+from app.routes.suppliers import suppliers_bp
+from app.routes.products import products_bp
 
 def create_app():
     app = Flask(__name__)
@@ -22,5 +24,14 @@ def create_app():
 
     from app.routes.test import test_bp
     app.register_blueprint(test_bp)
+
+    from app.routes.categories import categories_bp
+    app.register_blueprint(categories_bp)
+
+    from app.routes.suppliers import suppliers_bp
+    app.register_blueprint(suppliers_bp)
+
+    from app.routes.products import products_bp
+    app.register_blueprint(products_bp)
 
     return app
